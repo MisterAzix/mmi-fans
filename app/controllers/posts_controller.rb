f8908d2efe896abc5119939c3e8a26aa5b689a99
MisterAzix <flizi@live.fr>
2021-05-08 03:02:54 +0200

    Permit index view without sign in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  skip_before_action :authenticate_user!, only: :index
 
   # GET /posts or /posts.json
   def index
