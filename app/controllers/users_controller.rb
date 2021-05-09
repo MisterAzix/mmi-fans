7be170dbc0f77afa039a354ce0c8134ad635c03f
MisterAzix <flizi@live.fr>
2021-05-10 00:52:44 +0200

    Adding profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.where("lower(username) = ?", params[:slug].downcase).first
+    @can = @user == current_user || current_user.is_admin?
     @posts = Post.where(:user_id => @user.id).order('id DESC')
     @avatar = @user.avatar.attached? ? @user.avatar.url(:width=>256, :height=>256, :crop=>"fill") : "https://via.placeholder.com/128"
   end
