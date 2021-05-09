ab62c29a4659fbdb31578a6d1078d6662bb5aa07
MisterAzix <flizi@live.fr>
2021-05-10 00:28:25 +0200

    Adding user avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.where("lower(username) = ?", params[:slug].downcase).first
     @posts = Post.where(:user_id => @user.id).order('id DESC')
+    @avatar = @user.avatar.attached? ? @user.avatar.url(:width=>256, :height=>256, :crop=>"fill") : "https://via.placeholder.com/128"
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :likes, dependent: :destroy
+  has_one_attached :avatar
 
   def is_admin?
     self.admin == true
