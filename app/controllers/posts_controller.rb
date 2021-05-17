ed040bdd0e283419cfe4b0cc502af967f7f834ee
MisterAzix <flizi@live.fr>
2021-05-18 00:20:16 +0200

    Fix post display order on index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order('id DESC')
     @top = []
     User.all.each do |user|
       @top << {
