28ad5bd85f1ab87a6d400637204eadcc2bee24ca
MisterAzix <flizi@live.fr>
2021-05-19 02:56:04 +0200

    Adding secure parameter to cloudinary link generation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @user = User.where("lower(username) = ?", params[:slug].downcase).first
     @can = @user == current_user || current_user.is_admin?
     @posts = Post.where(:user_id => @user.id).order('id DESC')
-    @avatar = @user.avatar.attached? ? @user.avatar.url(:width=>256, :height=>256, :crop=>"fill") : "https://via.placeholder.com/128"
+    @avatar = @user.avatar.attached? ? @user.avatar.url(:secure=>true, :width=>256, :height=>256, :crop=>"fill") : "https://via.placeholder.com/128"
   end
 
 end
