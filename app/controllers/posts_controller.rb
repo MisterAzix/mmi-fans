2175edfa35b596ec179d330cc98ff36fee1666c5
MisterAzix <flizi@live.fr>
2021-05-19 03:26:27 +0200

    Remove needed auth for post & user show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   # GET /posts or /posts.json
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   
   def show
     @user = User.where("lower(username) = ?", params[:slug].downcase).first
-    @can = @user == current_user || current_user.is_admin?
+    @can = @current_user && (@user == current_user || current_user.is_admin?)
     @posts = Post.where(:user_id => @user.id).order('id DESC')
     @avatar = @user.avatar.attached? ? @user.avatar.url(:secure=>true, :width=>256, :height=>256, :crop=>"fill") : "https://via.placeholder.com/128"
   end
